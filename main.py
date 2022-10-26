929b5f8ff34cc7b675e40c7e81b7515a6158f67e
aizimpamvu <hallyiizza200@gmail.com>
2022-10-26 17:46:16 +0200

    add new

--- main.py
+++ main.py
@@ -24,9 +24,12 @@ class InternetSpeedTwitterBot:
 
     def get_internet_speed(self):
         self.driver.get("https://www.speedtest.net/")
+        time.sleep(10)
+
 
         go_button = self.driver.find_element(By.XPATH,
-                                             '//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[1]/a/span[4]')
+                                             '//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[1]/a/span[1]')
+        print("Hello")
         go_button.click()
         time.sleep(60)
 
